aaf0b0c9bc85e2daa7bc3dca8344c559ce24c304
GavinMendelGleason <jacobian@gmail.com>
2015-09-17 17:58:38 +0100

    Adding xsdParser, and the new dataType checking for full instance checking of triples.

--- applications/aux.pl
+++ applications/aux.pl
@@ -1,6 +1,7 @@
 :- module(aux,[]).
 :- use_module(library(semweb/rdf_db)).
 :- use_module(schemaRules).
+:- use_module(xsdParser).
 % This file carries auxilliary predicates that need to be used
 % for reasoning tests.
 
@@ -27,63 +28,58 @@ collect(X,[H|T],Graph) :-
     rdf(X,rdf:first,H,Graph),
     rdf(X,rdf:rest,Y,Graph),
     collect(Y,T,Graph).
-
-:- rdf_meta subsumes(r,r,r,o,o).
-subsumes(CC,CC,_).
-subsumes(CC,CP,Schema) :-
-    rdf(CC,rdfs:subClassOf,CZ,Schema),
-    subsumes(CZ,CP).
-subsumes(CC,CP,Schema) :-
-    rdf(CZ,owl:unionOf,ListObj, Schema),
-    collect(ListObj,L,Schema),
-    member(CC,L),
-    subsumes(CZ,CP).
-subsumes(CC,CP,Schema) :-
-    rdf(CC,owl:intersectionOf,ListObj,Schema),
-    collect(ListObj,L,Schema),
-    member(CZ,L),
-    subsumes(CZ,CP).
     
-:- rdf_meta valid(r,r,r,o,o).
-valid(X,CC,Instance,Schema) :-
-    setof(CZ,rdf(CC,rdfs:subClassOf,CZ,Schema),L),
-    forall(member(C,L), 
-	   valid(X,C,Instance,Schema)). 
-valid(X,CC,_,Schema) :-
+:- rdf_meta invalid(r,r,o,o,t).
+invalid(X,CC,Instance,Schema,Reason) :-
+    rdf(CC,rdfs:subClassOf,CZ,Schema),
+    invalid(X,CZ,Instance,Schema,Reason).
+invalid(X,CC,_,Schema,Reason) :-
     rdf(CC,rdfs:oneOf,ListObj,Schema),
     collect(ListObj,L,Schema),
-    member(X,L).
-valid(X,CC,Instance,Schema) :-
+    \+ member(X,L),
+    Reason = [reason='Not an element of enumeration (oneOf)',
+	      element=X,
+	      class=CC].
+invalid(X,CC,Instance,Schema,Reason) :-
     rdf(CC,owl:intersectionOf,ListObj,Schema),
     collect(ListObj,L,Schema),
-    forall(member(C,L),valid(X,C,Instance,Schema)).
-valid(X,CC,Instance,Schema) :-
+    member(C,L),
+    invalid(X,C,Instance,Schema, Reason).
+invalid(X,CC,Instance,Schema,Reason) :-
     rdf(CC,owl:unionOf,ListObj, Schema),
     collect(ListObj,L,Schema),
-    member(C,L),
-    valid(X,C,Instance,Schema).
-valid(X,CC,Instance,Schema) :-
-    rdf(CC,owl:complementOf,CN,Schema), \+ valid(X,CN,Instance,Schema).
-valid(X,CC,Instance,Schema) :-
-    rdf(CC,owl:'Restriction', CR, Schema),
-    eltRestriction(X,CR,Instance,Schema).
-valid(_,CC,_,Schema) :-
-    \+ hasFurtherConstraint(CC,Schema).  % we are already subsumed, so also valid
-                                         % as there are no further constraints
+    forall(member(C,L),
+	   invalid(X,C,Instance,Schema,_)),
+    Reason = [reason='No element of union is valid',
+	      element=X,
+	      class=CC].
+invalid(X,CC,Instance,Schema, Reason) :-
+    rdf(CC,owl:complementOf,CN,Schema),
+    \+ invalid(X,CN,Instance,Schema,_),
+    Reason = [reason='Complement is valid',
+	      element=X,
+	      class=CC].
+invalid(X,CC,Instance,Schema,Reason) :-
+    rdf(CC, rdf:type, owl:'Restriction', Schema),
+    neltRestriction(X,CC,Instance,Schema,Reason).
 
-hasFurtherConstraint(CC,Schema) :-
-    rdf(CC,rdfs:subClassOf,_,Schema).
-hasFurtherConstraint(CC,Schema) :-
-    rdf(CC,owl:oneOf,_,Schema).
-hasFurtherConstraint(CC,Schema) :-
-    rdf(CC,owl:intersectionOf,_,Schema).
-hasFurtherConstraint(CC,Schema) :-
-    rdf(CC,owl:unionOf,_,Schema).
-hasFurtherConstraint(CC,Schema) :-
-    rdf(CC,owl:complementOf,_,Schema).
-hasFurtherConstraint(CC,Schema) :-
-    rdf(CC,owl:'Restriciton',_,Schema).
-       
+%%%%% May not be relevant anymore
+%% %% invalid(_,CC,_,Schema) :-
+%% %%    \+ hasFurtherConstraint(CC,Schema).  % we are already subsumed, so also valid
+%% %%                                         % as there are no further constraints
+
+%% %% hasFurtherConstraint(CC,Schema) :-
+%% %%     rdf(CC,rdfs:subClassOf,_,Schema).
+%% %% hasFurtherConstraint(CC,Schema) :-
+%% %%     rdf(CC,owl:oneOf,_,Schema).
+%% %% hasFurtherConstraint(CC,Schema) :-
+%% %%     rdf(CC,owl:intersectionOf,_,Schema).
+%% %% hasFurtherConstraint(CC,Schema) :-
+%% %%     rdf(CC,owl:unionOf,_,Schema).
+%% %% hasFurtherConstraint(CC,Schema) :-
+%% %%     rdf(CC,owl:complementOf,_,Schema).
+%% %% hasFurtherConstraint(CC,Schema) :-
+%% %%     rdf(CC,owl:'Restriciton',_,Schema).
 
 runChain(X,[P],Y,Instance,Schema) :-
     inferredEdge(X,P,Y,Instance,Schema).
@@ -91,11 +87,7 @@ runChain(X,[P|PropList],Z,Instance,Schema) :-
     inferredEdge(X,P,Y,Instance,Schema),
     runChain(Y,PropList,Z,Instance,Schema).
 
-leProp(SOP,SOP,_).
-leProp(SOP,POP,Schema) :-
-    rdf(SOP,rdfs:subPropertyOf,POP,Schema).
-
-% Impose an ordering to avoid cycles.
+% Impose an ordering to avoid non-termination (subproperty ordering)
 % Concrete links are already in InferredEdge
 inferredTransitiveEdge(X,OP,Z,Instance,Schema) :-
     rdf(SOP,rdfs:subPropertyOf,OP,Schema),
@@ -117,75 +109,203 @@ inferredEdge(X,OP,Y,Instance,Schema) :-
     rdf(SOP,rdfs:subPropertyOf,OP,Schema),
     inferredEdge(X,SOP,Y,Instance,Schema).
 
-card(X,OP,Y,N) :-
-    (setof(Y,inferredEdge(X,OP,Y), ListX) *-> ListX = L ; L = []),
+card(X,OP,Y,Instance,Schema,N) :-
+    (setof(Y,inferredEdge(X,OP,Y,Instance,Schema), ListX) *-> ListX = L ; L = []),
     length(L,N).
 
 qualitifiedCard(X,OP,Y,C,Instance,Schema,N) :-
-    (setof(Y,(inferredEdge(X,OP,Y),
+    (setof(Y,(inferredEdge(X,OP,Y,Instance,Schema),
 	      elt(Y,C,Instance,Schema)),
 	   ListX) *-> ListX = L ; L = []),
     length(L,N).
 
-:- rdf_meta eltRestriction(r,r,o,o).
-eltRestriction(X,CR,Instance,Schema) :-
+:- rdf_meta neltRestriction(r,r,o,o,t).
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:someValuesFrom,C,Schema),
-    inferredEdge(X,OP,Y,Instance,Schema),
-    elt(Y,C,Instance,Schema).
-eltRestriction(X,CR,Instance,Schema) :-
+    forall(inferredEdge(X,OP,Y,Instance,Schema),
+           (\+ nelt(Y,C,Instance,Schema,_))),
+    Reason = [reason='No values from restriction class',
+	      element=X,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:allValuesFrom,C,Schema),
-    forall(inferredEdge(X,OP,Y,Instance,Schema),
-	   elt(Y,C,Instance,Schema)).
-eltRestriction(X,CR,_,Schema) :-
+    inferredEdge(X,OP,Y,Instance,Schema),
+    nelt(Y,C,Instance,Schema,_),
+    Reason = [reason='Some values not from restriction class',
+	      element=X,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:minCardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
     atom_number(CardStr,N),
-    card(X,OP,_,M),
-    M >= N.
-eltRestriction(X,CR,_,Schema) :-
+    card(X,OP,_,Instance,Schema,M),
+    M < N, atom_number(A,M),
+    Reason = [reason='Cardinality too small on restriction class',
+	      element=X,
+	      cardinality=A,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:maxCardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
     atom_number(CardStr,N),
-    card(X,OP,_,M),
-    N >= M.
-eltRestriction(X,CR,_,Schema) :-
+    card(X,OP,_,Instance,Schema,M),
+    N < M, atom_number(A,M),
+    Reason = [reason='Cardinality too large on restriction class',
+	      element=X,
+	      cardinality=A,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:cardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
     atom_number(CardStr,N),
-    card(X,OP,_,N).
-eltRestriction(X,CR,Instance,Schema) :-
+    card(X,OP,_,Instance,Schema,M),
+    N \= M, atom_number(A,M),
+    Reason = [reason='Cardinality unequal on restriction class',
+	      element=X,
+	      cardinality=A,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:minQualifiedCardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
     rdf(CR,owl:onClass,C,Schema),
     atom_number(CardStr,N),
     qualifiedCard(X,OP,_,C,Instance,Schema,M),
-    M >= N.
-eltRestriction(X,CR,Instance,Schema) :-
+    M < N, atom_number(A,M),
+    Reason = [reason='Qualified Cardinality too small on restriction class',
+	      element=X,
+	      cardinality=A,
+	      qualifiedOn=C,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
     rdf(CR,owl:maxQualifiedCardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
     rdf(CR,owl:onClass,C,Schema),
     atom_number(CardStr,N),
     qualifiedCard(X,OP,_,C,Instance,Schema,M),
-    N >= M.
-eltRestriction(X,CR,Instance,Schema) :-
+    N < M, atom_number(A,M),
+    Reason = [reason='Qualified Cardinality too large on restriction class',
+	      element=X,
+	      cardinality=A,
+	      qualifiedOn=C,
+	      class=CR].
+neltRestriction(X,CR,Instance,Schema,Reason) :-
     rdf(CR,owl:onProperty,OP,Schema),
-    rdf(CR,owl:cardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
+    rdf(CR,owl:qualifiedCardinality,literal(type(xsd:nonNegativeInteger, CardStr)),Schema),
     rdf(CR,owl:onClass,C,Schema),
     atom_number(CardStr,N),
-    qualifiedCard(X,OP,_,C,Instance,Schema,N).
+    qualifiedCard(X,OP,_,C,Instance,Schema,N),
+    N \= M, atom_number(A,M),
+    Reason = [reason='Qualified Cardinality unequal on restriction class',
+	      element=X,
+	      cardinality=A,
+	      qualifiedOn=C,
+	      class=CR].
 
-:- rdf_meta class(r,o).
-class(X,Schema) :- rdf(X, rdf:type, rdfs:'Class', Schema).
-class(X,Schema) :- rdf(X, rdf:type, owl:'Class', Schema).
-class(X,Schema) :- rdf(X, rdf:type, owl:'Restriction', Schema).
+%% %% :- rdf_meta class(r,o).
+%% %% class(X,Schema) :- rdf(X, rdf:type, rdfs:'Class', Schema).
+%% %% class(X,Schema) :- rdf(X, rdf:type, owl:'Class', Schema).
+%% %% class(X,Schema) :- rdf(X, rdf:type, owl:'Restriction', Schema).
 
-:- rdf_meta elt(r,r,o,o).
-elt(X,CP,Instance,Schema) :-
+:- rdf_meta nsubsumes(r,r,r,o,o).
+subsumes(CC,CC,_).
+subsumes(CC,CP,Schema) :-
+    rdf(CC,rdfs:subClassOf,CZ,Schema),
+    subsumes(CZ,CP).
+subsumes(CC,CP,Schema) :-
+    rdf(CZ,owl:unionOf,ListObj, Schema),
+    collect(ListObj,L,Schema),
+    member(CC,L),
+    subsumes(CZ,CP).
+subsumes(CC,CP,Schema) :-
+    rdf(CC,owl:intersectionOf,ListObj,Schema),
+    collect(ListObj,L,Schema),
+    member(CZ,L),
+    subsumes(CZ,CP).
+
+:- rdf_meta nelt(r,r,o,o,t).
+nelt(X,CP,Instance,Schema,Reason) :-
     rdf(X, rdf:type, CC, Instance),
-    subsumes(CC,CP,Schema),
-    valid(X,CP,Instance,Schema).
+    (subsumes(CC,CP,Schema) *->
+       invalid(X,CC,Instance,Schema,Reason)
+     ; Reason = [reason='Subsumption Impossible',
+		 element=X,
+		 class=CP,
+		 instanceClass=CC]).
 
-%%%%verifyEdge(X,P,Instance,Schema) :- 
-%%%%    valid(X, CC, Instance, Schema).
+:- rdf_meta nbasetypeElt(r,r,t).
+nbasetypeElt(literal(S),xsd:string,Reason) :-
+    \+ atom(S), term_to_atom(S,A),
+    Reason = [reason='Expected atom, found term',
+	      literal=A].
+nbasetypeElt(literal(lang(S,L)),xsd:string,Reason) :-
+    \+ atom(S), term_to_atom(S,A),
+    Reason = [reason='Expected atom in string section, found term.',
+	      literal=A].
+nbasetypeElt(literal(lang(S,L)),xsd:string,Reason) :-
+    \+ atom(L), term_to_atom(L,A),
+    Reason = [reason='Expected atom in language section, found term.',
+	      literal=A].
+nbasetypeElt(literal(type(T,S)),xsd:string,Reason) :-
+    \+ atom(S), term_to_atom(type(T,S),A),
+    Reason = [reason='Expected atom, found term as element.',
+	      literal=A].
+nbasetypeElt(literal(type(T,S)),xsd:string,Reason) :-
+    \+ atom(T), term_to_atom(type(T,S),A),
+    Reason = [reason='Expected atom, found term as type.',
+	      literal=A].
+nbasetypeElt(literal(type(T1,S)),T2,Reason) :-
+    \+ dataTypeSubsumes(T1,T2), term_to_atom(T1,A), term_to_atom(T2,B),
+    Reason = [reason='Could not subsume type1 with type2',
+	      type1=A,
+	      type2=B].
+nbasetypeElt(literal(type(_,S)),xsd:boolean,Reason) :-
+    \+ member(S,['true','false','1','0']), term_to_atom(S,A),
+    Reason = [reason='Not a well formed boolean.',
+	      literal=A].
+nbasetypeElt(literal(type(_,S)),xsd:decimal,Reason) :-
+    \+ 
+nbasetypeElt(xsd:integer). 
+nbasetypeElt(xsd:double). 
+nbasetypeElt(xsd:float). 
+nbasetypeElt(xsd:time).
+nbasetypeElt(xsd:dateTime). 
+nbasetypeElt(xsd:dateTimeStamp).
+nbasetypeElt(xsd:gYear). 
+nbasetypeElt(xsd:gMonth). 
+nbasetypeElt(xsd:gDay). 
+nbasetypeElt(xsd:gYearMonth). 	
+nbasetypeElt(xsd:gMonthDay). 
+nbasetypeElt(xsd:duration). 
+nbasetypeElt(xsd:yearMonthDuration). 
+nbasetypeElt(xsd:dayTimeDuration). 
+nbasetypeElt(xsd:byte). 
+nbasetypeElt(xsd:short). 
+nbasetypeElt(xsd:int). 
+nbasetypeElt(xsd:long). 
+nbasetypeElt(xsd:unsignedByte). 
+nbasetypeElt(xsd:unsignedInt). 
+nbasetypeElt(xsd:unsignedLong). 
+nbasetypeElt(xsd:positiveInteger). 
+nbasetypeElt(xsd:nonNegativeInteger). 
+nbasetypeElt(xsd:negativeInteger). 
+nbasetypeElt(xsd:nonPositiveInteger). 
+nbasetypeElt(xsd:base64Binary). 
+nbasetypeElt(xsd:anyURI). 
+nbasetypeElt(xsd:language). 
+nbasetypeElt(xsd:normalizedString). 
+nbasetypeElt(xsd:token). 
+nbasetypeElt(xsd:'NMTOKEN'). 
+nbasetypeElt(xsd:'Name'). 
+nbasetypeElt(xsd:'NCName'). 
+nbasetypeElt(rdf:'PlainLiteral').
+nbasetypeElt(rdf:'Literal').
+
+% The triple (X,P,Y) comes from the Herbrand base.
+%:- rdf_meta invalidEdge(r,r,r,o,o,t).
+%invalidEdge(X,P,Y,Instance,Schema,Reason) :-
+%    rdf(X,P,Y,Instance), % Check to see if we were deleted or added.
+%    domain(P),
+%    subsumptionPropertiesOf(P,SuperP),
+%    subsumptionPropertiesOf(SubP,P).
